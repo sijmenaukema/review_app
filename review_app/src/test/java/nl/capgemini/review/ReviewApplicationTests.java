bf416b6f8e6209b98d117381b8b62ef9895d6027
sijmen <sijmenaukema@gmail.com>
2022-11-25 11:59:29 +0100

    first commit

---
+++ review_app/src/test/java/nl/capgemini/review/ReviewApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.capgemini.review;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReviewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
